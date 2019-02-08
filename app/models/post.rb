f84846700e8d585a390425e9488d7c8b1b53a5bc
Srija S <srijakumar7@gmail.com>
2019-02-08 14:43:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ActiveRecord::Base
-
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates :is_clickbait?
+  validate :is_clickbait?
 
   CLICKBAIT_PATTERNS = [
     /Won't Believe/i,
@@ -18,7 +17,4 @@ class Post < ActiveRecord::Base
       errors.add(:title, "must be clickbait")
     end
   end
-
-
-
 end
